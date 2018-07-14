929feb4817e6be96ffeba13a85291368087dcdc7
Felix <XeZrunner@users.noreply.github.com>
2018-07-14 14:28:43 +0200

    WebcamViewerX.ViewManagement project
    This project will be used to switch Views in the main application.
    Its implementation right now is pretty clean, and the main app will also try and use this in a simple way.
    WV9 would have the Pages, the Frames and the transitions hard-coded into the main app. That was a really bad implementation.

---
+++ WebcamViewerX/WebcamViewerX.ViewManagement/PageGatherer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WebcamViewerX.ViewManagement
+{
+    /// <summary>
+    /// This class serves its purpose to gather the Page objects for the Views.
+    /// It's a seperate class so the ViewManager.cs doesn't get messy.
+    /// </summary>
+    public class PageGatherer
+    {
+        public Page GetViewPage(View view)
+        {
+            switch (view.DevName)
+            {
+                default:
+                    return new Page();
+
+                case "Home":
+                    return new Page()
+                    {
+                        Content = new TextBlock() { HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center, Text = "Home", FontSize = 18 }
+                    };
+
+                case "Settings":
+                    return new Page()
+                    {
+                        Content = new TextBlock() { HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center, Text = "Settings", FontSize = 18 }
+                    };
+            }
+        }
+    }
+}
---
+++ WebcamViewerX/WebcamViewerX.ViewManagement/View.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WebcamViewerX.ViewManagement
+{
+    public enum ViewTitlebarBehavior
+    {
+        ExtendsUnderTitlebar,
+        ExtendsAboveTitlebar,
+        ExtendsBeyondWindow,
+        NoTitlebar
+    }
+
+    public class View
+    {
+        /// <summary>
+        /// The developer name for the View.
+        /// </summary>
+        public string DevName;
+
+        /// <summary>
+        /// The title of the View used in UI.
+        /// </summary>
+        public string Title;
+
+        /// <summary>
+        /// The View's titlebar behavior.
+        /// </summary>
+        public ViewTitlebarBehavior TitlebarBehavior;
+
+        /// <summary>
+        /// Determines whether the View has its own transition animations.
+        /// </summary>
+        public bool HasTransitionAnimations()
+        {
+            object anim_in = Page.TryFindResource("Anim_In");
+            object anim_out = Page.TryFindResource("Anim_In");
+
+            if (anim_in != null & anim_out != null)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// The Page object itself.
+        /// </summary>
+        public Page Page;
+    }
+}
