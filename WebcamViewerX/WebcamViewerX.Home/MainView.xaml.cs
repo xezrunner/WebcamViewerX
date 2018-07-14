c8055e231ce02e2a175fecc3ba1073acae4aee4b
Felix <XeZrunner@users.noreply.github.com>
2018-07-14 15:05:45 +0200

    WebcamViewerX.Home project
    The Home View's project.
    Serves as a demo and placeholder for now.
    
    The Subpages folder will host, well, subpages, such as a webcam editor right from the home page, or an image sizing dialog etc...

---
+++ WebcamViewerX/WebcamViewerX.Home/MainView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebcamViewerX.Home
+{
+    public partial class MainView : Page
+    {
+        Theming.ThemeManager ThemeManager;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+            ThemeManager = new Theming.ThemeManager(themeDictionary); // initialize theme manager
+        }
+
+        #region Theming
+
+        public void ThemeChangeHandler(object sender, EventArgs e)
+        {
+            themeDictionary.MergedDictionaries.Clear();
+            themeDictionary.MergedDictionaries.Add((ResourceDictionary)sender);
+        }
+
+        #endregion
+    }
+}
