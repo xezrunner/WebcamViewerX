c3a2fc4cdcb5bb34e73ec36c4fd3e84814d7be90
Felix <XeZrunner@users.noreply.github.com>
2018-08-03 12:48:32 +0200

    Settings/User Interface: debug subview for UI

--- WebcamViewerX/WebcamViewerX/Settings/Subviews/UserInterfaceSubView.xaml.cs
+++ WebcamViewerX/WebcamViewerX/Settings/Subviews/UserInterfaceSubView.xaml.cs
@@ -17,12 +17,78 @@ namespace WebcamViewerX.Settings.Subviews
 {
     public partial class UserInterfaceSubView : Page
     {
+        MainWindow MainWindow = (MainWindow)Application.Current.MainWindow;
         Theming.ThemeManager ThemeManager;
 
         public UserInterfaceSubView()
         {
             InitializeComponent();
             ThemeManager = new Theming.ThemeManager(themeDictionary);
+        }
+
+        XeZrunner.UI.Configuration.Config Config = XeZrunner.UI.Configuration.Config.Default;
+        Theming.Config Theme_Config = Theming.Config.Default;
+
+        private void main_Loaded(object sender, RoutedEventArgs e)
+        {
+            switch (Theme_Config.theme)
+            {
+                case "Light":
+                    theme_0.IsChecked = true; break;
+                case "Dark":
+                    theme_1.IsChecked = true; break;
+            }
+
+            switch (Config.controlfx)
+            {
+                case "P":
+                    controlfx_P.IsChecked = true; break;
+                case "MM":
+                    controlfx_MM.IsChecked = true; break;
+                case "Reveal":
+                    controlfx_Reveal.IsChecked = true; break;
+            }
+        }
+
+        void ValidateThemeChanges()
+        {
+            foreach (RadioButton button in themeStackPanel.Children)
+            {
+                if (button.IsChecked.Value)
+                    Theme_Config.theme = (string)button.Content;
+            }
+        }
+
+        void ValidateControlFXChanges()
+        {
+            foreach (RadioButton button in controlfxStackPanel.Children)
+            {
+                if (button.IsChecked.Value)
+                    Config.controlfx = (string)button.Content;
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Theme_Config.Save();
+            Config.Save();
+
+            MainWindow.ShowTextDialog("Changes saved",
+                "Current config values: \n\n" +
+                "theme: " + Theme_Config.theme + "\n" +
+                "accent: " + "" + "\n" +
+                "controlfx: " + Config.controlfx + "\n"
+                );
+        }
+
+        private void theme_Click(object sender, RoutedEventArgs e)
+        {
+            ValidateThemeChanges();
+        }
+
+        private void controlfx_Click(object sender, RoutedEventArgs e)
+        {
+            ValidateControlFXChanges();
         }
     }
 }
